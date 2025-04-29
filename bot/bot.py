e4550de505e9cd13f43eb35b7bba8a1c6f96522b
MrCode200 <navidyaghmaei@gmail.com>
2025-04-29 18:30:45 +0200

    No file selected

--- bot/bot.py
+++ bot/bot.py
@@ -311,11 +311,7 @@ class OracleLinkBot:
             time.sleep(2)
             df = fetch_klines(symbol=symbol, interval=interval, limit=75)
             df = df.iloc[:-1]
-            if df.empty:
-                await context.bot.send_message(
-                    chat_id=chat_id,
-                    text=f"{df}"
-                )
+            await context.bot.send_message(chat_id=chat_id, text=f"{df}")
 
             # STT
             stt_conf, stt_data = stt.evaluate(df)
