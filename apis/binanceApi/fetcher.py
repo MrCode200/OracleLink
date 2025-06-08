1da5c364c753d654b07e0663192b8657aca63736
MrCode200 <navidyaghmaei@gmail.com>
2025-06-08 13:28:58 +0200

    chore(paperTrading): remove paper trading module
    Remove the entire paper trading module from the project.
    
    - Remove all paper trading related files.
    - Remove `mainPaperTrade.py`
    - Clean up project structure.

--- apis/binanceApi/fetcher.py
+++ apis/binanceApi/fetcher.py
@@ -1,3 +1,5 @@
+import time
+
 from binance.client import Client
 import pandas as pd
 
@@ -28,7 +30,12 @@ def fetch_klines(symbol: str, interval: str, limit: int) -> pd.DataFrame:
 
     # Create DataFrame
     df = pd.DataFrame(klines, columns=columns)
-    df.drop(columns=['Ignore'], inplace=True)
+    df.drop(columns=['Ignore'])
+
+    # Convert 'Open Time' and 'Close Time' to datetime and set as index
+    df['Close Time'] = pd.to_datetime(df['Close Time'], unit='ms')
+    df['Open Time'] = pd.to_datetime(df['Open Time'], unit='ms')
+    df.set_index('Open Time', drop=False)
 
     # Convert price and volume columns to float
     numeric_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
@@ -37,8 +44,6 @@ def fetch_klines(symbol: str, interval: str, limit: int) -> pd.DataFrame:
     return df
 
 if __name__ == '__main__':
-    df = fetch_klines(symbol='BTCUSDT', interval='1m', limit=100)
-    candle = df.iloc[-1]
-
-    for col, value in candle.items():
-        print(f"{col}: {value}; {type(value)}")
+    df1 = fetch_klines(symbol='BTCUSDT', interval='1m', limit=1)
+    df2 = fetch_klines(symbol='BTCUSDT', interval='1m', limit=1)
+    print(df1.iloc[-1]['Open Time'] == df2.iloc[-1]['Open Time'])
--- paperTrading/core/__init__.py
+++
@@ -1,1 +0,0 @@
-from .paperTrader import PaperTrader
